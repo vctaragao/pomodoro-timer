78987d8d5a23a7e591c71e74094f21e2b088ce08
Victor Moraes de Aragão <vct.aragao@unifei.edu.br>
2020-05-19 01:35:54 -0300

    Winning tasks of the day

---
+++ src/components/WinningTasks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="winning_tasks_container">
+    <h2>Vitórias para o dia!</h2>
+    <div class="task_group">
+      <input type="checkbox" />
+      <input type="text" placeholder="Objetivo 1" />
+    </div>
+    <div class="task_group">
+      <input type="checkbox" />
+      <input type="text" placeholder="Objetivo 2" />
+    </div>
+    <div class="task_group">
+      <input type="checkbox" />
+      <input type="text" placeholder="Objetivo 3" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WinningTasks"
+};
+</script>
+
+<style>
+.winning_tasks_container {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+</style>
