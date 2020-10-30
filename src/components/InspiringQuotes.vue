24c7dec492b7b5be10c49b3df92fa515d5ba99aa
Victor Moraes <vct.aragao@gmail.com>
2020-10-29 23:43:15 -0300

    Fix do timer e melhorias de feedback para o usuario

--- src/components/InspiringQuotes.vue
+++ src/components/InspiringQuotes.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="inspiring_quotes_container">
     <div v-if="quote" class="tso_blackcard">
-      {{quote.quote}}
-      <span>Author: {{quote.author}}</span>
+      {{ quote.quote }}
+      <span>Author: {{ quote.author }}</span>
     </div>
     <div
       class="light-text"
-      style="text-decoration: none; color:#ccc; z-index: 999; text-align: right; padding-right: 20px; margin-top: 5px;"
+      style="
+        text-decoration: none;
+        color: #ccc;
+        z-index: 999;
+        text-align: right;
+        padding-right: 20px;
+        margin-top: 5px;
+      "
     >
       <a
-        style="text-decoration: none; color:#ccc;"
+        style="text-decoration: none; color: #ccc"
         title="Inspiring Quote of the day"
         href="https://theysaidso.com/quote-of-the-day/inspire"
-      >They Said So</a>
+        >They Said So</a
+      >
     </div>
   </div>
 </template>
@@ -23,22 +31,21 @@ export default {
   name: "InspiringQuotes",
   data() {
     return {
-      quote: ""
+      quote: "",
     };
   },
   mounted() {
     if (this.$cookies.isKey("quote")) this.quote = this.$cookies.get("quote");
     else {
-      console.log("Making the request");
       axios
         .get("https://quotes.rest/qod.json?category=inspire")
-        .then(response => {
+        .then((response) => {
           this.quote = response.data.contents.quotes[0];
           this.$cookies.set("quote", this.quote, "1d");
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     }
-  }
+  },
 };
 </script>
 
--- src/components/PomodoroTimer.vue
+++ src/components/PomodoroTimer.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="pomodoro_timer_container">
+    <transition name="fade-in" mode="out-in">
+      <h4 v-if="is_short_break_timer" key="short">Vá beber uma água</h4>
+      <h4 v-else-if="is_long_break_timer" key="long">
+        Você merece um descanso
+      </h4>
+      <h4 v-else-if="is_pomodoro_running" key="focus">Foco!</h4>
+      <h4 v-else key="default">Vamo nessa?</h4>
+    </transition>
     <div class="timer center">
-      <span>{{clock}}</span>
+      <span>{{ clock }}</span>
     </div>
     <button class="btn waves-effect waves-light" @click="toggleTimer()">
       <span v-if="is_pomodoro_running" class="material-icons">pause</span>
@@ -22,24 +30,29 @@ export default {
       timer: {
         minutes: 25,
         seconds: 60,
-        current_time: 0
+        current_time: 0,
       },
       pomodoro: {
         session: 1,
+        session_period: 25,
         short_break: 5,
-        long_break: 25
+        long_break: 25,
       },
       animation: null,
-      newSession: false,
       is_long_break_timer: false,
       is_short_break_timer: false,
-      is_pomodoro_running: false
+      is_pomodoro_running: false,
+      notification_audio: null,
+      finished_audio: null,
     };
   },
   methods: {
     checkCookies() {
       if (this.$cookies.isKey("pomodoro_conf")) {
-        this.timer.minutes = this.$cookies.get("pomodoro_conf").minutes;
+        this.pomodoro.session_period = this.$cookies.get(
+          "pomodoro_conf"
+        ).minutes;
+        this.timer.minutes = this.pomodoro.session_period;
         this.pomodoro.short_break = this.$cookies.get(
           "pomodoro_conf"
         ).short_break;
@@ -49,40 +62,36 @@ export default {
       }
     },
     toggleTimer() {
-      if (this.is_pomodoro_running) this.stopTimer();
-      else this.startTimer();
+      if (this.is_pomodoro_running) {
+        this.stopTimer();
+      } else {
+        this.startTimer();
+      }
     },
     hasPomodoroFinished() {
-      if (this.pomodoro.session <= 4) {
-        return false;
-      }
-      this.pomodoro.session = 1;
-      this.is_pomodoro_running = false;
-      return true;
-    },
-    startLongBreak() {
-      this.timer.minutes = +this.pomodoro.long_break;
+      return this.pomodoro.session < 4 ? false : true;
     },
     startNewSession() {
       ++this.pomodoro.session;
-      this.timer.minutes = 1;
-    },
-    startNewPomodoro() {
-      this.timer.minutes = 1;
+      this.timer.minutes = this.pomodoro.session_period;
+      this.notification_audio.play();
+      this.startTimer();
     },
     startTimer() {
-      if (this.timer.current_time) this.animation.set(this.timer.current_time);
-      else {
+      if (this.timer.current_time) {
+        this.animation.set(this.timer.current_time);
+      } else {
         this.timer.current_time = 0;
         this.animation.set(0);
       }
       this.animation.animate(1);
       this.is_pomodoro_running = true;
+      this.timer.current_time = 0;
     },
     stopTimer() {
+      this.is_pomodoro_running = false;
       this.timer.current_time = this.animation.value();
       this.animation.set(this.timer.current_time);
-      this.is_pomodoro_running = false;
     },
     createProgresbar(duration = 25) {
       this.animation = new ProgressBar.Circle(".timer", {
@@ -93,7 +102,7 @@ export default {
           let seconds_past = Math.floor(duration * 60 * animation.value());
           this.timer.seconds =
             60 - (seconds_past - Math.floor(seconds_past / 60) * 60);
-        }
+        },
       });
     },
     toggleBreakTimer() {
@@ -102,25 +111,34 @@ export default {
         this.timer.minutes = this.pomodoro.long_break;
       else if (this.is_short_break_timer)
         this.timer.minutes = this.pomodoro.short_break;
-      else this.timer.minutes;
+      else this.timer.minutes = this.pomodoro.session_period;
 
       this.createProgresbar(this.timer.minutes);
       this.startTimer();
     },
     resetTimer() {
-      this.animation.set(1);
+      if (this.hasPomodoroFinished()) {
+        this.finished_audio.play();
+      }
+      this.animation.destroy();
       this.timer.minutes = this.$cookies.isKey("pomodoro_conf")
         ? this.$cookies.get("pomodoro_conf").minutes
-        : 25;
+        : this.pomodoro.session_period;
       this.pomodoro.short_break = this.$cookies.isKey("pomodoro_conf")
         ? this.$cookies.get("pomodoro_conf").short_break
-        : 5;
+        : this.pomodoro.short_break;
       this.pomodoro.long_break = this.$cookies.isKey("pomodoro_conf")
         ? this.$cookies.get("pomodoro_conf").long_break
-        : 25;
+        : this.pomodoro.long_break;
+
+      this.pomodoro.session = 1;
+      this.is_long_break_timer = false;
+      this.is_short_break_timer = false;
       this.timer.current_time = 0;
       this.is_pomodoro_running = false;
-    }
+      this.createProgresbar(this.timer.minutes);
+      this.animation.set(1);
+    },
   },
   computed: {
     clock() {
@@ -131,24 +149,29 @@ export default {
       let m =
         this.timer.minutes < 10 ? "0" + this.timer.minutes : this.timer.minutes;
       return m + ":" + s;
-    }
+    },
   },
   watch: {
     is_short_break_timer() {
-      this.toggleBreakTimer();
+      if (this.is_short_break_timer) {
+        this.toggleBreakTimer();
+        this.notification_audio.play();
+      }
     },
     is_long_break_timer() {
-      this.toggleBreakTimer();
+      if (this.is_long_break_timer) {
+        this.toggleBreakTimer();
+        this.notification_audio.play();
+      }
     },
     "timer.seconds"() {
       if (this.timer.seconds === 60 && !this.timer.minutes) {
-        this.newSession = true;
         this.animation.set(0);
         if (this.hasPomodoroFinished()) {
           this.is_long_break_timer = !this.is_long_break_timer;
+
           if (!this.is_long_break_timer) {
-            this.startNewPomodoro();
-            this.stopTimer();
+            this.resetTimer();
           }
         } else {
           this.is_short_break_timer = !this.is_short_break_timer;
@@ -156,17 +179,22 @@ export default {
             this.startNewSession();
           }
         }
-        this.newSession = false;
       } else if (this.timer.seconds === 59) {
         --this.timer.minutes;
       }
-    }
+    },
+    clock() {
+      document.title = `Sessão ${this.pomodoro.session}ª: ${this.clock}`;
+    },
   },
   mounted() {
     this.checkCookies();
     this.createProgresbar(this.timer.minutes);
     this.animation.set(1);
-  }
+
+    this.notification_audio = new Audio(require("../assets/notification.mp3"));
+    this.finished_audio = new Audio(require("../assets/finished.mp3"));
+  },
 };
 </script>
 
@@ -179,7 +207,7 @@ export default {
 
 .pomodoro_timer_container {
   position: absolute;
-  top: 50%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 20%;
@@ -215,5 +243,21 @@ export default {
 .center {
   margin-left: auto;
   margin-right: auto;
+}
+
+.fade-in-enter,
+.fade-in-leave-to {
+  opacity: 0;
+}
+
+.fade-in-enter-to,
+.fade-in-leave {
+  opacity: 1;
+}
+
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: opacity 2s ease-in;
+  transition-delay: 10s;
 }
 </style>
